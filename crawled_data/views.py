72491d58a8a56a66d9c2a9d3e017ced60d6bf6f6
seodaun <daun4799@naver.com>
2020-04-14 00:23:29 +0900

    modify parser

--- crawled_data/views.py
+++ crawled_data/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import BoardData 
-# Create your views here.
- 
+from .parser import naver_search_list_crawled
+# Create your views here. 
 def naver_search_list(request):
+    naver_search_list_crawled()
     datas = BoardData.objects.all()
     return render(request, 'index.html', {'datas': datas})
