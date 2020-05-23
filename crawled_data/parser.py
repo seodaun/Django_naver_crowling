d1861cd2265d0fd8bc743a650a7a3aa92ba7ca8b
seodaun <daun4799@naver.com>
2020-05-23 22:14:07 +0900

    modify debug setting

--- crawled_data/parser.py
+++ crawled_data/parser.py
@@ -1,31 +1,34 @@
 
+from crawled_data.models import BoardData
+import django
 import requests
 
 from bs4 import BeautifulSoup
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CrawlingDjango.settings")
- 
-import django
+
 django.setup()
-from crawled_data.models import BoardData 
-def naver_search_list_crawled(): 
+
+
+def naver_search_list_crawled():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
-    url = 'https://datalab.naver.com/keyword/realtimeList.naver?where=main'
+    url = 'https://datalab.naver.com/keyword/realtimeList.naver'
     res = requests.get(url, headers=headers)
-    soup = BeautifulSoup(res.content, 'html.parser') 
-    datas = {'title':soup.select('span.item_title'), 'lank':soup.select('span.item_num')}
-    queryset = BoardData.objects.all()  
+    soup = BeautifulSoup(res.content, 'html.parser')
+    datas = {'title': soup.select(
+        'span.item_title'), 'lank': soup.select('span.item_num')}
+    queryset = BoardData.objects.all()
     for data in queryset:
         data.delete()
-    for t,l in zip(datas['title'],datas['lank']):
-       BoardData(title = t.text, lank = l.text).save() 
+    for t, l in zip(datas['title'], datas['lank']):
+        BoardData(title=t.text, lank=l.text).save()
 
 
 # if __name__=='__main__':
 #     blog_data_dict = naver_search_list_crawled()
-#     queryset = BoardData.objects.all()  
+#     queryset = BoardData.objects.all()
 #     for data in queryset:
 #         data.delete()
 #     for t,l in zip(blog_data_dict['title'],blog_data_dict['lank']):
-#        BoardData(title = t.text, lank = l.text).save() 
+#        BoardData(title = t.text, lank = l.text).save()
